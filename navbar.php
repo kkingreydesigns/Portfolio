50424ae3083f2467ace5e700b83ba5fa1d2c9bea
Karyn Kingrey <kkingreydesigns@gmail.com>
2026-02-04 22:07:08 -0700

    made navbar php

---
+++ navbar.php
@@ -0,0 +1,25 @@
+  <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto gap-4">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="about.html">About</a>
+              <a class="nav-link" href="#">Projects</a>
+              <a class="nav-link" href="#">Blog</a>
+              <a class="nav-link" href="#">Contact</a>
+            </div>
+          </div>
+        </div>
+      </nav>
